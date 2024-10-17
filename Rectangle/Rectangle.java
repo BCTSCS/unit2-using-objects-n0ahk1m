cbe300f38b90067fce43fae4e725ec8f79606eea
n0ahk1m <noahmatiaskim@gmail.com>
2024-10-17 13:58:52 -0400

    updated rectangle

--- Rectangle/Rectangle.java
+++ Rectangle/Rectangle.java
@@ -3,30 +3,55 @@ import greenfoot.*;
 public class Rectangle extends Actor {
   private int width;
   private int height;
+  // instant variable of reference type
+  private Rectangle partner;
   
   public Rectangle(int w, int h){
-    width = w;
-    height = h;
+    setWidth(w,h);
     getImage().scale(width*10, height*10);
   }
   
   public Rectangle(int s){
-    width = s;
-    height = s;
+    setWidth(s);
     getImage().scale(width*10, height*10);
   }
   
   public Rectangle(){
-    width = 10;
-    height = 10;
+    partner = new Rectangle(10,5);
+    System.out.println("Width:"+width+"Height"+height+"Partner"+partner);
     getImage().scale(width*10, height*10);
   }
   
+  public void setWidth(){
+      width = 10;
+      height = 10;
+  }
+  
+  public void setWidth(int size){
+      width = size;
+      height = size;
+  }
+  
+  public void setWidth(int w, int h){
+      width = w;
+      height = h;
+  }
+  
+  public int getArea(){
+      int area = width*height;
+      return area;
+  }
+  
+  public static void printArea(){
+      int area = width*height;
+      System.out.println(area);
+  }
+  
   public static void main(String[] args){
-    Rectangle rect1 = null;
-    System.out.println(rect1);
-    rect1 = new Rectangle(12,10);
-    System.out.println(rect1);
+    //Rectangle rect1 = null;
+    //System.out.println(rect1);
+    //rect1 = new Rectangle(12,10);
+    //System.out.println(rect1);
   }
   
   public String toString(){
